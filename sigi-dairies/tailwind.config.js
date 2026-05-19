f9dc05060b80ff439b10ea81d2ce65c13686fe1a
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-05-20 00:04:49 +0300

    Initailize project and set tailwind configurations

---
+++ sigi-dairies/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#FC7500",
+        green: "#71CB1E",
+        white: "#FEFEFE",
+        dark: "#000E1C",
+        brown: "#C37B59",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
